09f586ad5cc310c63439607bff78483630368464
Jyoti <jyotikiran12@gmail.com>
2021-02-15 14:48:10 +0530

    Add todo form

---
+++ src/addToDo.js
@@ -0,0 +1,32 @@
+const addToDoForm = (() => {
+
+  const todoForm = (()=> {
+    const todoContainer = document.getElementById('todo-form');
+    const todoForm = document.createElement('form');
+    todoForm.innerHTML = `
+        <label for="title">Title:</label><br>
+        <input type="text" id="title" name="title" required><br>
+        <label for="description">Description:</label><br>
+        <input type="text" id="description" name="description" required><br>
+        <label for="duedate">Due Date:</label><br>
+        <input type="date" id="duedate" name="duedate" required><br>
+        <label for="priority">Priority:</label><br>
+        <select name="priority" id="priority">
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select><br>
+        <input type="submit" value="Add"></input>
+    `;
+    todoContainer.appendChild(todoForm);
+  });
+
+  const hideForm = (() => {
+    document.getElementById('todo-form').style.display = 'none';
+  });
+
+  return { hideForm, todoForm };
+})();
+
+export default addToDoForm;
+
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import ProjectForm from './newProject';
 import './addProject';
 import Project from './addProject';
 import Projects from './project.js';
+import addToDoForm from './addToDo.js'
 
-window.addEventListener('DOMContentLoaded', ProjectForm.hideForm);
+window.addEventListener('DOMContentLoaded', ProjectForm.hideForm, addToDoForm.hideForm);
 
 //selectors
 const addSign = document.querySelector('#add-project');
 const form = document.getElementById('form');
 const viewProjectBtn = document.getElementById('view-project');
+// const todoForm = document.getElementById('todo-form');
+const todobtn = document.getElementById('add-todo');
 
 //functions
 const clearContent = () => {
@@ -42,3 +45,8 @@ viewProjectBtn.addEventListener('click', () => {
   // clearContent();
   // ProjectForm.viewAllProject();
 });
+
+todobtn.addEventListener('click', () => {
+  addToDoForm.todoForm(); 
+})
+
