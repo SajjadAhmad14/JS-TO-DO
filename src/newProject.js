f9caade79819392807030540cc13bb2c51bcacba
Jyoti <jyotikiran12@gmail.com>
2021-02-17 18:01:14 +0530

    Add function to get the clicked project name

--- src/newProject.js
+++ src/newProject.js
@@ -1,5 +1,6 @@
 import Projects from './project.js';
 import clearContent from './index.js'
+import { LoaderTargetPlugin } from 'webpack';
 
 const ProjectForm = (() => {
 
@@ -17,18 +18,33 @@ const ProjectForm = (() => {
     for(let i=0; i<Projects.length; i++) {
       const h2 = document.createElement('h2');
       h2.classList.add('project-list');
-      h2.textContent = Projects[i].title;
+      const anchor = document.createElement('a');
+      anchor.setAttribute('class','project-link');
+      anchor.textContent = Projects[i].title;
+      h2.appendChild(anchor);
       projectList.appendChild(h2);
     }
-    const items = document.getElementsByClassName('project-list');
-    for (let i = 0; i < items.length; i++) {
-      items[i].addEventListener('click', () => {
-        console.log()
-      })
-    }
+    // const items = document.getElementsByClassName('project-list');
+    // for (let i = 0; i < items.length; i++) {
+    //   items[i].addEventListener('click', () => {
+    //     console.log()
+    //   })
+    // }
     // h2.addEventListener('click', () => {
     //   showTasks(Projects[i].items, h2);
-    // });
+    // // });
+    // })
+    const al = document.querySelector('#all-projects');
+    al.addEventListener('click', projectTodo)
+  });
+
+  const projectTodo = ((e) => {
+    let x = e.target;
+    // let test = x.textContent;
+    // for(let i=0; i<Projects.length; i++) {
+    //   if()
+    // }
+    console.log(e.target);
   });
 
   const showTasks = (item, h2) => {
--- webpack.config.js
+++ webpack.config.js
@@ -24,5 +24,5 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
-  },
+  }
 };
