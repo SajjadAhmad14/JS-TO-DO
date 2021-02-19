6f42fa9f55aed5af6736371c7a837ae59b18cf01
Sajjad Ahmad <jogimar14@gmail.com>
2021-02-19 18:54:43 +0500

    delete every item from items array by clicking on delete icon

--- src/newProject.js
+++ src/newProject.js
@@ -36,12 +36,12 @@ const ProjectForm = (() => {
     let test = x.textContent;
     for (let i = 0; i < Projects.length; i++) {
       if (Projects[i].title === e.target.textContent) {
-        projectItemsTable(Projects[i].items);
+        projectItemsTable(Projects[i].items, Projects[i]);
       }
     }
   });
 
-  const projectItemsTable = ((projectItemsArray) => {
+  const projectItemsTable = ((projectItemsArray, project) => {
     const table = document.getElementById('todo-table');
     const tbleBody = document.getElementById('table-body');
     for (let i = 0; i < projectItemsArray.length; i++) {
@@ -50,27 +50,31 @@ const ProjectForm = (() => {
       const td0 = document.createElement('td');
       td0.textContent = i + 1;
       const td1 = document.createElement('td');
-      td1.textContent = projectItemsArray[i].title
+      td1.textContent = project.title;
       const td2 = document.createElement('td');
-      td2.textContent = projectItemsArray[i].dueDate
+      td2.textContent = projectItemsArray[i].title
       const td3 = document.createElement('td');
+      td3.textContent = projectItemsArray[i].dueDate
+      const td4 = document.createElement('td');
       const editBtn = document.createElement('button');
       editBtn.innerHTML = `<i class="fa fa-pencil-square-o" aria-hidden="true"></i>`;
       editBtn.classList.add('btn', 'btn-success');
       editBtn.setAttribute('type', 'button');
-      td3.appendChild(editBtn);
-      const td4 = document.createElement('td');
+      td4.appendChild(editBtn);
+      const td5 = document.createElement('td');
+      td5.setAttribute('id', 'delete-list')
       const dltBtn = document.createElement('button');
       dltBtn.textContent = 'X';
       dltBtn.classList.add('btn', 'btn-danger');
       dltBtn.setAttribute('type', 'button');
-      td4.appendChild(dltBtn);
+      td5.appendChild(dltBtn);
 
       row.appendChild(td0);
       row.appendChild(td1);
       row.appendChild(td2);
       row.appendChild(td3);
       row.appendChild(td4);
+      row.appendChild(td5);
       tbleBody.appendChild(row);
 
     }
@@ -81,10 +85,32 @@ const ProjectForm = (() => {
       if (ele.classList.contains('btn-danger')) {
         ele.parentElement.parentElement.remove();
         let index = ele.parentElement.parentElement.firstChild.textContent;
+        index = Number(index);
+        const elem = ele.parentElement.parentElement.lastChild;
+        const siblings = allSiblings(elem);
+        const title = siblings[1].textContent;
+        for (let i = 0; i < Projects.length; i++) {
+          if (title == Projects[i].title) {
+            Projects[i].items.splice(index - 1, 1);
+          }
+        }
       }
     });
 
   });
+
+  const allSiblings = function (elem) {
+    let siblings = [];
+    let sibling = elem.parentNode.firstChild;
+    while (sibling) {
+      if (sibling.nodeType === 1 && sibling !== elem) {
+        siblings.push(sibling);
+      }
+      sibling = sibling.nextSibling
+    }
+
+    return siblings;
+  };
 
   const showTasks = (item, h2) => {
     const listContainer = document.createElement('div');
